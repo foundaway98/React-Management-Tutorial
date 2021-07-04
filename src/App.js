0ca87809d0ce9b2276853019da3add81d39a3225
foundaway98 <foundaway98@naver.com>
2021-07-04 11:48:34 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <header className="App-header">
+        <img src={logo} lat="logo" />
+        <h2>자~드가자~</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
