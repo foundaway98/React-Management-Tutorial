46ffd161985383f69006a11fb1a40a0e65405f21
foundaway98 <foundaway98@naver.com>
2021-07-30 00:49:37 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers=[
-  {
-    'id': 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : '장주찬',
-    'birthday' : '980318',
-    'gender' : '남자',
-    'job' : '커플'
-  },
-  {
-    'id': 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '박성수',
-    'birthday' : '980209',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {
-    'id': 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : '양혁진',
-    'birthday' : '280809',
-    'gender' : '남자',
-    'job' : '할아버지'
-  }
-]
-
 class App extends Component {
+
+  state={
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi().then(res => this.setState({customers: res}))
+    .catch(err => console.log(err))
+  }
+
+  callApi = async()=> {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+
   render() {
     const {classes}=this.props;
     return (
@@ -64,7 +54,9 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); })}
+              {this.state.customers ? this.state.customers.map(c => { 
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+               }) : ""}
             </TableBody>
           </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+          'id': 1,
+          'image' : 'https://placeimg.com/64/64/1',
+          'name' : '장주찬',
+          'birthday' : '980318',
+          'gender' : '남자',
+          'job' : '커플'
+        },
+        {
+          'id': 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '박성수',
+          'birthday' : '980209',
+          'gender' : '남자',
+          'job' : '대학생'
+        },
+        {
+          'id': 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '양혁진',
+          'birthday' : '280809',
+          'gender' : '남자',
+          'job' : '할아버지'
+        }
+      ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
