e824c5656a358751af784c914d2a5314e45a898f
foundaway98 <foundaway98@naver.com>
2021-08-10 22:49:54 +0900

    Create Customer Database Table & node.js Connect

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+})
+connection.connect();
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-          'id': 1,
-          'image' : 'https://placeimg.com/64/64/1',
-          'name' : '장주찬',
-          'birthday' : '980318',
-          'gender' : '남자',
-          'job' : '커플'
-        },
-        {
-          'id': 2,
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : '박성수',
-          'birthday' : '980209',
-          'gender' : '남자',
-          'job' : '대학생'
-        },
-        {
-          'id': 3,
-          'image' : 'https://placeimg.com/64/64/3',
-          'name' : '양혁진',
-          'birthday' : '280809',
-          'gender' : '남자',
-          'job' : '할아버지'
-        }
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
